c59475eeb9481a0adbb7975d3461039d81749ad8
Stephen Kelly <stevekm@users.noreply.github.com>
2017-04-19 12:01:12 -0400

    fix downstream setup script

--- downstream_NGS580_WES_analysis.sh
+++ downstream_NGS580_WES_analysis.sh
@@ -19,47 +19,55 @@ results_ID="$(basename "$project_subdir")"
 # ~~~~~ LOCATIONS & FILES & SETTINGS ~~~~~ #
 analysis_dir="/ifs/data/molecpathlab/NGS580_WES"
 scripts_dir="/ifs/data/molecpathlab/scripts"
-coverage_analysis_dir="${scripts_dir}/sns-wes-coverage-analysis"
-downstream_analysis_dir="${scripts_dir}/sns-wes-downstream-analysis"
+source_coverage_analysis_dir="${scripts_dir}/sns-wes-coverage-analysis"
+source_downstream_analysis_dir="${scripts_dir}/sns-wes-downstream-analysis"
 timestamp="$(date +"%Y-%m-%d_%H-%M-%S")"
 divider="---------------------------"
-analysis_project_results_dir="${analysis_dir}/${project_subdir}"
 
+analysis_project_results_dir="${analysis_dir}/${project_subdir}"
+analysis_project_coverage_dir="${analysis_project_results_dir}/sns-wes-coverage-analysis"
+analysis_project_downstream_dir="${analysis_project_results_dir}/sns-wes-downstream-analysis"
 
 # ~~~~~ VALIDATIONS ~~~~~ #
+printf "\n%s\nValidating project:\n%s\n\n" "$divider" "$analysis_project_results_dir"
+
 # make sure sequencing project dir exists
-printf "\n%s\nValidating run..." "$divider"
+printf "\nMaking sure directory exists...\n"
 [ ! -d "$analysis_project_results_dir" ] && printf "ERROR: Project directory does not exist:\n%s\n\n" "$analysis_project_results_dir"
 
 # make sure it contains summary-combined.wes.csv file (some samples finished samples)
+printf "\nMaking sure sns pipeline analysis has completed...\n"
 [ -z "$(find "$analysis_project_results_dir" -name "summary-combined.wes.csv" -print -quit)" ] && printf "ERROR: No summary-combined.wes.csv file found in directory:\n%s\n\n" "$sequencer_project_dir"
 
 # ~~~~~ COVERAGE ANALYSIS ~~~~~ #
-printf "\n%s\nStarting coverage analysis.." "$divider"
-zip_filename="$(echo "${project_subdir}" | tr "/" "_")"
+printf "\n%s\nStarting coverage analysis..\n" "$divider"
+
 # copy the analysis scripts and run them, then zip the TSV and BED outputs
 (
-rsync -vhPtr "$coverage_analysis_dir" "${analysis_project_results_dir}/"
-cd "${analysis_project_results_dir}/sns-wes-coverage-analysis"
-ln -fs "$analysis_project_results_dir" run_analysis_output
+# set -x
+mkdir -p "$analysis_project_coverage_dir"
+zip_filename="$(echo "${project_subdir}" | tr "/" "_")"
+rsync -vhPtr "$source_coverage_analysis_dir/" "$analysis_project_coverage_dir/"
+cd "$analysis_project_coverage_dir"
+ln -fs "../" "run_analysis_output"
 ./calculate_average_coverages.R
 zip "${zip_filename}.zip" *.tsv *.bed
 )
-
 # ~~~~~ REPORTING ~~~~~ #
 printf "\n%s\nStarting reporting analysis.." "$divider"
 (
 module load pandoc/1.13.1
 
-rsync -vhPtr "$downstream_analysis_dir" "${analysis_project_results_dir}/"
-cd "${analysis_project_results_dir}/sns-wes-downstream-analysis"
+mkdir -p "$analysis_project_downstream_dir"
+rsync -vhPtr "${source_downstream_analysis_dir}/" "${analysis_project_downstream_dir}/"
+cd "${analysis_project_downstream_dir}"
 
 printf "%s" "$project_ID" > project_ID.txt
 printf "%s" "$results_ID" > results_ID.txt
 
-set -x
-ln -fs ../ run_analysis_output
-ln -fs ../sns-wes-coverage-analysis sns-wes-coverage-analysis
+# set -x
+ln -fs "../" "run_analysis_output"
+ln -fs "../sns-wes-coverage-analysis" "sns-wes-coverage-analysis"
 
 report_file="${project_ID}_${results_ID}_analysis_report.Rmd"
 /bin/cp analysis_report.Rmd "$report_file"
