0e671b47f5d793f33bb33e833a4f083189185488
Stephen Kelly <stevekm@users.noreply.github.com>
2017-05-01 13:15:45 -0400

    Update demultiplex-NGS580-WES.sh

--- demultiplex-NGS580-WES.sh
+++ demultiplex-NGS580-WES.sh
@@ -9,6 +9,7 @@
 ## /opt/illumina/Isas/1.26.0/bcl2fastq2/bin/bcl2fastq  --ignore-missing-bcls --ignore-missing-filter --ignore-missing-positions --ignore-missing-controls --auto-set-to-zero-barcode-mismatches --find-adapters-with-sliding-window --adapter-stringency 0.9 --mask-short-adapter-reads 35 --minimum-trimmed-read-length 35 -R "/data/scratch/workspace/RunFolder" --sample-sheet "/data/scratch/workspace/RunFolder/SampleSheet.csv" -o "/data/scratch/workspace/RunFolder/Analysis/Temp_01.01-GenerateFASTQ.FastqGeneration"
 
 ## Using bcl2fastq 2.17.1
+# copied from Igor's script
 
 # make the output group-writeable
 umask 007
