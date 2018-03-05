78eedddcdb04326686cd2b60780611eceacf2bc6
Stephen Kelly <stevekm@users.noreply.github.com>
2018-03-05 13:20:55 -0500

    update db run script

--- samples_database/run.py
+++ samples_database/run.py
@@ -4,10 +4,12 @@
 Sets up and updates a SQLite database for tracking samples in runs
 """
 import os
+import sys
 import find
 import sqlite3
+from util import samplesheet
 from util import sqlite_tools as sqt
-
+from util import tools
 # ~~~~~ FUNCTIONS ~~~~~ #
 def get_runs(seq_dir):
     """
@@ -23,6 +25,8 @@ def setup_db(conn):
     sqt.create_table(conn = conn, table_name = "runs", col_name = "run", col_type = "TEXT", is_primary_key = True)
     sqt.add_column(conn = conn, table_name = "runs", col_name = "path", col_type = "TEXT")
     sqt.add_column(conn = conn, table_name = "runs", col_name = "samplesheet", col_type = "TEXT")
+    sqt.create_table(conn = conn, table_name = "settings", col_name = "run", col_type = "TEXT", is_primary_key = True) # each run has one settings entry
+    sqt.create_table(conn = conn, table_name = "samples", col_name = "hash", col_type = "TEXT", is_primary_key = True) # multiple samples per run
 
 def find_samplesheet(run_dir):
     """
@@ -31,51 +35,126 @@ def find_samplesheet(run_dir):
     matches = find.find(search_dir = run_dir, inclusion_patterns = ['SampleSheet.csv'], search_type = "file")
     return(matches)
 
+def load_samplesheet_data(samplesheet_file):
+    """
+
+    Examples
+    --------
+    Example usage::
+
+        x = "SampleSheet.csv"
+        run_settings, run_samples = load_samplesheet_data(x)
+
+    """
+    data = samplesheet.IEMFile(path = samplesheet_file)
+    run_settings = {}
+    run_settings.update(data.data['Reads'])
+    run_settings.update(data.data['Header'])
+    run_settings.update(data.data['Settings'])
+    run_samples = data.data['Data']['Samples']
+    return((run_settings, run_samples))
+
+def update_db_run(conn, path, run):
+    """
+    Update a single run in the db
+
+    Parameters
+    ----------
+    conn: sqlite3.Connection object
+        connection object to the database
+    path: str
+        path to the run directory
+    run: str
+        Name of the run
+
+    """
+    # find the samplesheet
+    samplesheet_matches = find_samplesheet(run_dir = path)
+    # validate search results
+    if len(samplesheet_matches) > 1:
+        print("ERROR: multiple files were found; {0}".format(samplesheet_matches))
+        raise
+    elif len(samplesheet_matches) < 1:
+        print("WARNING: no SampleSheet files were found in dir; {0}".format(path))
+        return(None)
+    else:
+        # update the runs table
+        samplesheet_file = samplesheet_matches.pop()
+        print("Adding samplesheet: {0}".format(samplesheet_file))
+        row = {'run': run, 'path': path, 'samplesheet': samplesheet_file}
+        sqt.sqlite_insert(conn = conn, table_name = "runs", row = row)
+        # get the sample information from the sheet
+        run_settings, run_samples = load_samplesheet_data(samplesheet_file)
+        # update sample information & add to db
+        run_settings['run'] = run
+        run_settings = sqt.sanitize_dict_keys(d = run_settings)
+        for key in run_settings.keys():
+            sqt.add_column(conn = conn, table_name = "settings", col_name = key, col_type = "TEXT")
+
+        sqt.sqlite_insert(conn = conn, table_name = "settings", row = run_settings)
+
+        for run_sample in run_samples:
+            run_sample['run'] = run
+            run_sample['hash'] = sqt.md5_str(''.join(run_sample.values()))
+            run_sample = sqt.sanitize_dict_keys(d = run_sample)
+            for key in run_sample.keys():
+                sqt.add_column(conn = conn, table_name = "samples", col_name = key, col_type = "TEXT")
+            sqt.sqlite_insert(conn = conn, table_name = "samples", row = run_sample)
+
 def update_db_runs(conn, run_dirs):
     """
     Check the database to make sure all run dirs are present with samplesheets
     """
     for path in run_dirs:
+        # run ID = dir basename
         run = os.path.basename(path)
+        # check if the run is already in the db
         if not sqt.row_exists(conn = conn, table_name = "runs", col_name = "run", value = run):
+            # find the samplesheet
             print('Run dir doesnt exist in database: {0}; searching for samplesheet...'.format((run, path)))
-            samplesheet_matches = find_samplesheet(run_dir = path)
-            if len(samplesheet_matches) > 1:
-                print("ERROR: multiple files were found; {0}".format(samplesheet_matches))
-                raise
-            elif len(samplesheet_matches) < 1:
-                print("WARNING: no files were found in dir; {0}".format(path))
-                continue
-            else:
-                samplesheet = samplesheet_matches.pop()
-                print("Adding samplesheet: {0}".format(samplesheet))
-                row = {'run': run, 'path': path, 'samplesheet': samplesheet}
-                sqt.sqlite_insert(conn = conn, table_name = "runs", row = row)
+            update_db_run(conn = conn, path = path, run = run)
+        # TODO: fix this doesn't work due to SQLite restrictions
+        # elif force_update:
+        #     print('Run dir already exists in database, forcing update for: {0}; searching for samplesheet...'.format((run, path)))
+        #     update_db_run(conn = conn, path = path, run = run)
         else:
             print('Run dir already exists in database: {0}; skipping...'.format((run, path)))
 
-
+force_update = False
 # ~~~~~ RUN ~~~~~ #
-db_file = "nextseq.sqlite"
-# connect to db
-conn = sqlite3.connect(db_file)
-setup_db(conn = conn)
+if __name__ == '__main__':
+    args = sys.argv[1:]
+    try:
+        force_val = args.pop(0)
+    except:
+        force_val = None
+    if force_val == "force":
+        force_update = True
 
-# directory with sequencer output
-seq_dir = '/ifs/data/molecpathlab/quicksilver/'
+    db_file = "nextseq.sqlite"
+    # connect to db
+    conn = sqlite3.connect(db_file)
+    setup_db(conn = conn)
 
-# get the list of available runs
-run_dirs = get_runs(seq_dir = seq_dir)
+    # directory with sequencer output
+    seq_dir = '/ifs/data/molecpathlab/quicksilver/'
 
-# check the database for each run to see if the samplesheet needs to be added
-update_db_runs(conn = conn, run_dirs = run_dirs)
+    # get the list of available runs
+    run_dirs = get_runs(seq_dir = seq_dir)
 
-# create csv dumps of database
-table_names = sqt.get_table_names(conn = conn)
-for name in table_names:
-        output_file = "{0}.csv".format(name)
-        sqt.dump_csv(conn = conn, table_name = name, output_file = output_file)
+    # check the database for each run to see if the samplesheet needs to be added
+    update_db_runs(conn = conn, run_dirs = run_dirs)
 
-# ~~~~~ CLEAN UP ~~~~~ #
-conn.commit()
-conn.close()
+    # dump the entire database
+    db_dump_file = os.path.join(os.path.dirname(db_file), '{0}.dump.txt'.format(os.path.basename(db_file)))
+    sqt.dump_sqlite(conn = conn, output_file = db_dump_file)
+
+    # create csv dumps of database
+    table_names = sqt.get_table_names(conn = conn)
+    for name in table_names:
+            output_file = "{0}.csv".format(name)
+            sqt.dump_csv(conn = conn, table_name = name, output_file = output_file)
+
+    # ~~~~~ CLEAN UP ~~~~~ #
+    conn.commit()
+    conn.close()
